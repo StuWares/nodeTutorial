3a1977f72a72fab171c2a2b8d003f869746a8694
Stu Wares <Stu Wares>
2017-05-10 22:11:47 +0100

    first practice using exports

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,8 +1,15 @@
-console.log("Starting app.");
+console.log("Starting app.js");
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+var added = notes.add(6, 4);
+console.log(added);
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`);
+// var res = notes.addNote();
+// console.log(res);
+
+// var user = os.userInfo();
+
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
